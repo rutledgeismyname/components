1f43e25e5a72651d27a095a11083c4c9e713cc70
rutledgeismyname <chrisrutledge@Chriss-MacBook-Pro.local>
2021-03-03 14:29:00 -0500

    update components page v3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.tooltipped');
-    var instances = M.Tooltip.init(elems, options);
-    
+'use strict';
+// Dark / light mode declare
+const lightMode = document.querySelector('.fa-sun-o');
+const darkMode = document.querySelector('.fa-moon-o');
+
+// Toggle Light Mode
+lightMode.addEventListener('click', function () {
+document.querySelector('.body').classList.add('light');
+document.querySelector('.fa-sun-o').style.display = "none";
+document.querySelector('.fa-moon-o').style.display = "block";
+});
+
+// Toggle Dark Mode
+darkMode.addEventListener('click', function () {
+  document.querySelector('.body').classList.remove('light');
+  document.querySelector('.fa-sun-o').style.display = "block";
+  document.querySelector('.fa-moon-o').style.display = "none";
   });
+  
