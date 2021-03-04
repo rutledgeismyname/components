604d0bd01381a24952d72296ef1c5945de6f8247
rutledgeismyname <chrisrutledge@Chriss-MacBook-Pro.local>
2021-03-03 19:10:13 -0500

    add in more dark mode

--- js/main.js
+++ js/main.js
@@ -2,18 +2,37 @@
 // Dark / light mode declare
 const lightMode = document.querySelector('.fa-sun-o');
 const darkMode = document.querySelector('.fa-moon-o');
+const cardClass = document.querySelectorAll('.card');
+const cardTitles = document.querySelectorAll('.card-title');
+var i;
 
 // Toggle Light Mode
 lightMode.addEventListener('click', function () {
-document.querySelector('.body').classList.add('light-bg');
-document.querySelector('.fa-sun-o').style.display = "none";
-document.querySelector('.fa-moon-o').style.display = "block";
+  document.querySelector('.body').classList.add('light-mode');
+  document.querySelector('.fa-sun-o').style.display = "none";
+  document.querySelector('.fa-moon-o').style.display = "block";
+  // For loop for all card colors
+  for (i = 0; i < cardClass.length; i++){
+  cardClass[i].classList.add('light-card');
+  }
+  // For loop for all card titles
+  for (i = 0; i < cardTitles.length; i++){
+  cardTitles[i].classList.add('light-card-title');
+  }
 });
 
 // Toggle Dark Mode
 darkMode.addEventListener('click', function () {
-  document.querySelector('.body').classList.remove('light-bg');
+  document.querySelector('.body').classList.remove('light-mode');
   document.querySelector('.fa-sun-o').style.display = "block";
   document.querySelector('.fa-moon-o').style.display = "none";
-  });
+  // For loop for all card colors
+  for (i = 0; i < cardClass.length; i++){
+  cardClass[i].classList.remove('light-card');
+  }
+  // For loop for all card titles
+  for (i = 0; i < cardTitles.length; i++){
+  cardTitles[i].classList.remove('light-card-title');
+  }
+});
   
